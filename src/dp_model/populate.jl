b350e1ad432b2c391254306dd0bd5839dff84ca2
Bui Quang Minh <minhcly95@outlook.com>
2023-09-08 09:23:03 -0400

    Node population with filter

--- src/dp_model/populate.jl
+++ src/dp_model/populate.jl
@@ -1,15 +1,37 @@
-function populate_nodes!(dpgraph::DPGraph, solutions::Vector{BitSet}; include_arcs=true)
+function populate_nodes!(dpgraph::DPGraph, solutions::Vector{BitSet}; num_nodes=nothing, include_arcs=true)
     layers, arcs = dpgraph.layers, dpgraph.arcs
 
+    all_nodes = DPNode[]
+
+    # Get the nodes from solutions
     for sol in solutions
         path = unstructured_path(dpgraph, sol)
-        nodes = last.(dst.(path))
-        push!.(layers, nodes)
-        include_arcs && append!(arcs, path)
+        append!(all_nodes, dst.(path))
     end
 
+    # Filter nodes based on frequency
+    if isnothing(num_nodes)
+        unique!(all_nodes)
+    else
+        freq = countmap(all_nodes)
+        num_nodes = min(num_nodes, length(freq))
+        all_nodes = first.(sort(collect(freq), by=last, rev=true))[1:num_nodes]
+    end
+
+    # Add the nodes to corresponding layers
+    for (l, s) in all_nodes
+        push!(layers[l], s)
+    end
     unique!.(layers)
-    unique!(arcs)
+
+    # Add arcs from given solutions
+    if include_arcs
+        for sol in solutions
+            path = structured_path(dpgraph, sol)
+            append!(arcs, path)
+        end
+    end
+
     return dpgraph
 end
 
